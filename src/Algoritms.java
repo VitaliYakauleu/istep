a50a85a57774ea04ed610e9e46a40b16cac4909c
Vitali <itvitalikjob@gmail.com>
2023-11-27 19:45:32 +0300

    Intro

---
+++ src/Algoritms.java
@@ -0,0 +1,24 @@
+public class Algoritms {
+    public static void main(String[] args) {
+        System.out.println("Wake up");
+        System.out.println("Get up");
+        System.out.println("Make the bed");
+        System.out.println("Get dressed");
+        System.out.println("Drink a glass of water");
+        System.out.println("Brush teeth");
+// bra
+        int points = 70;
+        if (points<30)
+        System.out.println("Your mark is 3");
+        else if (points<50)
+            System.out.println("Your mark is 4");
+        else
+            System.out.println("Your mark is 5");
+        //Cyclic algorithm
+        for (int i=0; i<10; i++) {
+            System.out.print("Fall forward, ");
+            System.out.print("Push up, ");
+            System.out.println("Stand up");
+        }
+    }
+}
