2a771dc475ad8f1f19fa6b7f357eab86df1e3506
Vitali <itvitalikjob@gmail.com>
2024-01-11 20:17:19 +0300

    Intro

---
+++ src/Array/Sample5.java
@@ -0,0 +1,44 @@
+package Array;
+
+import java.util.Arrays;
+
+//Поместить в массив все простые трехзначные числа,
+// являющиеся палиндромами.
+public class Sample5 {
+    public static void main(String[] args) {
+        int count = countLength();
+        int[] arrayInt = new int[countLength()];
+        arrayInt = makeArray(count);
+        System.out.println(Arrays.toString(makeArray(countLength())));
+
+    }
+    static boolean prime (int num){
+        if (num<2)
+            return false;
+        double s= Math.sqrt(num);
+        for (int i=2; i<=s;i++)
+            if (num%i==0)
+                return false;
+        return true;
+    }
+    static int countLength(){
+        int count = 0;
+        for (int num = 100; num < 1000; num++) {
+            if (num/100==num%10 && prime(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static int[] makeArray(int count){
+        int[] Palindromes = new int[count];
+        int index = 0;
+        for (int num = 100; num < 1000; num++) {
+            if (num/100==num%10 && prime(num)) {
+                Palindromes[index++] = num;
+
+            }
+        }
+        return Palindromes;
+    }
+}
